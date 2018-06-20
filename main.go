6ae61bc704162904952db7fc1a5df7360a74095f
phina <phiphiphina@gmail.com>
2018-06-20 17:45:59 +0800

    test

--- main.go
+++ main.go
@@ -38,22 +38,24 @@ func main() {
         for _, event := range events {
             if event.Type == linebot.EventTypeMessage {
                 switch message := event.Message.(type) {
-                case *linebot.TextMessage:
-                    reply_msg := fmt.Sprintf("Your Token: %s\nyou type: %s\nmessage id: %s\nuser_id:%s\ngroup_id:%s\nroom_id:%s", event.ReplyToken, message.Text, message.ID, event.Source.UserID, event.Source.GroupID, event.Source.RoomID)
-                    if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(reply_msg)).Do(); err != nil {
-                        log.Print(err)
-                    }
+                    case *linebot.TextMessage:
+
+                // case *linebot.TextMessage:
+                //     reply_msg := fmt.Sprintf("Your Token: %s\nyou type: %s\nmessage id: %s\nuser_id:%s\ngroup_id:%s\nroom_id:%s", event.ReplyToken, message.Text, message.ID, event.Source.UserID, event.Source.GroupID, event.Source.RoomID)
+                //     if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(reply_msg)).Do(); err != nil {
+                //         log.Print(err)
+                //     }
                 }
             }
         }
     })
 
-    router.POST("/mysql", func(c *gin.Context) {
+    router.POST("/push_message", func(c *gin.Context) {
         buf  := make([]byte, 1024)  
         n, _ := c.Request.Body.Read(buf) 
         body := string(buf[0:n])
 
-        if _, err := bot.PushMessage("R6dcb63709978fed802b24764686c3ea8", linebot.NewTextMessage(body)).Do(); err != nil {
+        if _, err := bot.PushMessage("Udd05c331c930a303fd997c1b509af8ea", linebot.NewTextMessage(body)).Do(); err != nil {
             log.Print(err)
         }
     })
