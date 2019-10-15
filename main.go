a08a190032738b0a29e1504c076dc6cf2b866184
phina <phiphiphina@gmail.com>
2019-10-15 15:50:38 +0800

    test

--- main.go
+++ main.go
@@ -37,7 +37,8 @@ func main() {
         for _, event := range events {
             if event.Type == linebot.EventTypeMessage {
                 switch message := event.Message.(type) {
-                    case *linebot.TextMessage:
+        	    log.Print(message)
+	            case *linebot.TextMessage:
 
                     reply_msg := fmt.Sprintf("Your Token: %s\nyou type: %s\nmessage id: %s\nuser_id:%s\ngroup_id:%s\nroom_id:%s", event.ReplyToken, message.Text, message.ID, event.Source.UserID, event.Source.GroupID, event.Source.RoomID)
                     if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(reply_msg)).Do(); err != nil {
